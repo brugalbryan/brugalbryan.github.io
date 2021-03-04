2a2cdf2bc9646dde10015e46b15a7eebbf9c092f
Bryan Brugal <rapid_08@hotmail.com>
2021-03-04 01:40:15 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        My name is Bryan Brugal and I'm going to master React!!
+        My name is Bryan Brugal and I'm going to master React!
         </p>
         <a
           className="App-link"
