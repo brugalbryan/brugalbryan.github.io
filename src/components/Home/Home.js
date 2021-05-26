f9df42b40fbd389f7639c9b672fa8d08ee20fbe2
Bryan Brugal <rapid_08@hotmail.com>
2021-05-26 14:41:06 -0400

    changing home image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import classes from "./Home.module.css";
 import nicepic from './../../img/nicepic.png';
-import maxresdefault from './../../img/maxresdefault.jpg'
+// import maxresdefault from './../../img/maxresdefault.jpg'
+import minato from './../../img/minato.jpg'
+
 
 const Home= () => {
     return ( 
@@ -10,7 +12,7 @@ const Home= () => {
                 <h1 className={classes.Hello}>Hello, World!</h1>
                 <h1>Welcome to my website</h1>
             </div>
-            <img className={classes.Maxresdefault} src={maxresdefault} alt="nicepic"></img>
+            <img className={classes.Minato} src={minato} alt="nicepic"></img>
             <img className={classes.Nicepic} src={nicepic} alt="nicepic"></img>
 
         </div>
